f2a3354c4f81abf006b79d931dc48ddbb6d39139
Howard Liu <zhihliu@stonybrook.edu>
2022-10-27 14:56:59 -0400

    add login form

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import Rentals from "./components/rentals";
 import NotFound from "./components/notFound";
 import NavBar from "./components/navBar";
 import React from "react";
-import MovieForm from './components/movieForm';
+import MovieForm from "./components/movieForm";
+import LoginForm from "./components/loginForm";
 
 function App() {
   return (
@@ -14,11 +15,12 @@ function App() {
       <NavBar />
       <main className='container'>
         <Switch>
+          <Route path='/login' component={LoginForm} />
           <Route path='/movies/:id' component={MovieForm} />
-          <Route path='/movies' component={Movies}></Route>
-          <Route path='/customers' component={Customers}></Route>
-          <Route path='/rentals' component={Rentals}></Route>
-          <Route path='/not-found' component={NotFound}></Route>
+          <Route path='/movies' component={Movies} />
+          <Route path='/customers' component={Customers} />
+          <Route path='/rentals' component={Rentals} />
+          <Route path='/not-found' component={NotFound} />
           <Redirect from='/' exact to='/movies' />
           <Redirect to='/not-found' />
         </Switch>
